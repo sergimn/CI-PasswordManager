888e960f655de7d8f55e0da15762fde189e7812f
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2017-12-21 19:00:28 +0100

    Afeigeix les headers de lock.c

--- lock.c
+++ lock.c
@@ -1,5 +1,32 @@
 #include"main.c" //S'ha de comentar per a compilacio
-#include"lock.c" //S'ha de comentar per a compilacio
 #include"aux.c"
 #include"rutines_GLCD.c"
 #include"customGLCD.c"
+#include"interrupts.c"
+
+#define amunt RB0
+#define avall RB1
+#define
+
+
+#define NDigitsPIN 4
+char PIN[NDigitsPIN+1] = {1, 2, 3, 4, 0x00};
+
+
+//Gestiona el lock i les interrupcions relacionades amb el lock
+void manage_lock();
+
+
+//Bloqueja el sistema en espera del pin correcte
+void lock();
+
+void manage_lock(){
+    disab_interup();
+    lock();
+    init_interrupts();
+}
+
+
+
+
+
