34f62646fda1d193377a3698c0023b2d0e6bd06f
Sergi Miralles <sergi.miralles@est.fib.upc.edu>
2017-12-21 20:18:27 +0100

    Afegeix la funcio de lock

--- lock.c
+++ lock.c
@@ -10,7 +10,7 @@
 
 
 #define NDigitsPIN 4
-char PIN[NDigitsPIN+1] = {1, 2, 3, 4, 0x00};
+char PIN[NDigitsPIN+1] = {'1', '2', '3', '4', 0x00};
 
 
 //Gestiona el lock i les interrupcions relacionades amb el lock
@@ -21,15 +21,52 @@ void manage_lock();
 void manage_lock(){
     disab_interup();
     lock();
-    init_interrupts();
+    enable_interrup();
 }
+
 
 
 void lock(){
-
     int i = 0;
+    char usrPIN[NDigitsPIN+1] = {'0', '0', '0', '0', 0x00};
+    int correct_psswd = 0;
+    while(correct_psswd == 0){
+        mostrar_lock(usrPIN[], i);
+        if(amunt){ //Augmentar numero
+            __delay_ms(10);
+            if(amunt){
+                while(amunt);
+                no_char_overflo(usrPIN[i]+1);
 
-
+            }
+        }
+        if(avall){ //Augmentar numero
+            __delay_ms(10);
+            if(avall){
+                while(avall);
+                no_char_overflo(usrPIN[i]-1);
+            }
+        }
+        if(acceptar){
+            __delay_ms(10);
+            if(acceptar){
+                while(acceptar);
+                ++i;
+            }
+        }
+        if(i == NDigitsPIN){
+            if(PIN != usrPIN){
+                i = 0;
+                clearGLCD(0, 7, 0, 127);
+            }
+            else{
+                correct_psswd = 1;
+                count = 0;
+            }
+        }
+    }
 }
+
+
 
 
